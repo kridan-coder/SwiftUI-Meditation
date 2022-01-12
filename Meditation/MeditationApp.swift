5ffbaa99deba564d952e330452e82debabedead7
Daniel <krivelevdaniil@gmail.com>
2022-01-12 11:47:22 +0700

    Initial Commit

---
+++ Meditation/MeditationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationApp.swift
+//  Meditation
+//
+//  Created by Developer on 12.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct MeditationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
